0e0c0244ae4d63fe7c5a5226ff2a7530a5551a97
ricardomenotti <ricardomenotti31@gmail.com>
2024-04-12 08:17:10 -0500

    basic auth added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,7 @@
-import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
 
 export default {
-  schema: './db/schema.ts',
+  schema: './lib/db/schema.ts',
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
---
+++ lib/auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+import type { NextAuthConfig } from 'next-auth';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import db from './db';
+const config = {
+  providers: [Github],
+  callbacks: {
+    jwt({ token, trigger, session }) {
+      if (trigger === 'update') token.name = session.user.name;
+      return token;
+    },
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+
+      const unprotectedPaths = ['/login'];
+
+      const isProtected = !unprotectedPaths.some((path) =>
+        nextUrl.pathname.startsWith(path)
+      );
+
+      if (isProtected && !isLoggedIn) {
+        const redirectUrl = new URL('api/auth/signin', nextUrl.origin);
+        redirectUrl.searchParams.append('callbackUrl', nextUrl.href);
+        return Response.redirect(redirectUrl);
+      }
+
+      return true;
+    },
+  },
+  adapter: DrizzleAdapter(db),
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
